bf7ef8f3febf79c4d83ccc9b1fc1ffbecabba210
Grace Shih <vocalise@gmail.com>
2018-06-06 01:39:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create(attributes)
+  def self.create(*attributes)
     binding.pry
     attributes.each {|key,value| student.send("#{key}=", value)}
   end
