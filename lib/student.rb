a24a160fc0715e197ebcf1caf9dd86f9bd5f18fb
Grace Shih <vocalise@gmail.com>
2018-06-06 01:24:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,14 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize(name, grade)
+  '''def initialize(name, grade)
     @name = name
     @grade = grade
-  end
+  end'''
+  
+  def initialize(attributes)
+    attributes.each {|key,value| self.send("#{key}=", value)}
+  end  
   
   def self.create_table
     sql = <<-SQL
